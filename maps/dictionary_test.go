b8c05eb35de65efa0cac16ff0a19079eac839989
huzixiaoshu <1038794174@qq.com>
2022-01-22 22:38:56 +0800

    feat: add maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -22,6 +22,44 @@ func TestSearch(t *testing.T) {
 
 }
 
+// func TestAdd(t *testing.T){
+//     dictionary := Dictionary{}
+// 	dictionary.Add("test","this is just a test")
+
+// 	want := "this is just a test"
+// 	got,err := dictionary.Search("test")
+// 	if err != nil{
+// 		t.Fatal("should find added word:",err)
+// 	}
+
+// 	if want != got{
+// 		t.Errorf("got '%s' want '%s'",got ,want)
+// 	}
+
+// }
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
+	}
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
@@ -30,10 +68,10 @@ func assertStrings(t *testing.T, got, want string) {
 
 }
 
-func assertError(t *testing.T, got ,want error){
+func assertError(t *testing.T, got, want error) {
 	t.Helper()
 
-	if got != want{
-		t.Errorf("got error '%s' want '%s'",got,want )
+	if got != want {
+		t.Errorf("got error '%s' want '%s'", got, want)
 	}
 }
