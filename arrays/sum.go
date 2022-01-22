8d09d727b2b279d93326a57ac544ef934ffe369e
huzixiaoshu <1038794174@qq.com>
2022-01-22 10:58:25 +0800

    feat: support input Slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
+
 	return sum
 }
 
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,12 +4,15 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if want != got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
