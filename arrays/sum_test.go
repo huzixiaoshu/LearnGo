6cef9ec592a13e99fb40921b5628ff0614326551
huzixiaoshu <1038794174@qq.com>
2022-01-22 16:11:49 +0800

    feat: add

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -31,3 +31,27 @@ func TestSumAll(t *testing.T) {
 	}
 
 }
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		// reflect is unsafe
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
+}
