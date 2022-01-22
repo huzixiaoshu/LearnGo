b421dc40a575d67bc1551fee6ed3bafb416950f5
huzixiaoshu <1038794174@qq.com>
2022-01-22 14:13:47 +0800

    feat: add Circle

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,8 +1,14 @@
 package main
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
 }
 
 func Perimeter(rectangle Rectangle) float64 {
@@ -11,4 +17,12 @@ func Perimeter(rectangle Rectangle) float64 {
 
 func Area(rectangle Rectangle) float64 {
 	return rectangle.Width * rectangle.Height
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
